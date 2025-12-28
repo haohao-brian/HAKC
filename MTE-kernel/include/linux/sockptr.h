88da2ac3db9a52931932d719489272e5d67a5e33
tso0427 <b11902124@ntu.edu.tw>
2025-12-28 15:36:28 +0800

    updating MTE-kernel/include/linux/sockptr.h

--- MTE-kernel/include/linux/sockptr.h
+++ MTE-kernel/include/linux/sockptr.h
@@ -52,7 +52,7 @@ static inline int copy_from_sockptr_offset(void *dst, sockptr_t src,
 
 static inline int copy_from_sockptr(void *dst, sockptr_t src, size_t size)
 {
-	dst = (void *)((s64)dst | (0xFFFF << 48));
+	dst = check_hakc_data_access(dst,0x20001);
 	return copy_from_sockptr_offset(dst, src, 0, size);
 }
 
